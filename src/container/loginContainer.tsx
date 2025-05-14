743b4fd7dffbdd264c52e064b96fe8d6f835db4b
mdkhalidraza <khalidrazakhan@live.com>
2025-05-14 15:52:53 +0530

    KK: On Login screen If auth is processing show loading state

--- src/container/loginContainer.tsx
+++ src/container/loginContainer.tsx
@@ -2,12 +2,14 @@ import { Button } from "antd";
 import { GoogleOutlined } from "@ant-design/icons";
 import getGCPLogin from "../api/auth";
 import React from "react";
+import { useAppSelector } from "../slice";
 // import { useNavigate } from "react-router-dom";
 
 const LoginContainer = () => {
   //   const navigation = useNavigate();
 
   const [loading, setLoading] = React.useState(false);
+  const isLoading = useAppSelector((a) => a.auth.loading);
   const authInit = async () => {
     try {
       setLoading(true);
@@ -22,7 +24,11 @@ const LoginContainer = () => {
   return (
     <>
       <div className="login-container">
-        <Button loading={loading} icon={<GoogleOutlined />} onClick={authInit}>
+        <Button
+          loading={loading || isLoading}
+          icon={<GoogleOutlined />}
+          onClick={authInit}
+        >
           Login with Google
         </Button>
       </div>
